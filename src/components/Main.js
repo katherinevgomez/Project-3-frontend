3b44865d1ca11765d81d31a4cd35687f91b09416
gedunlap <g.e.dunlap90@gmail.com>
2021-05-21 16:20:18 -0400

    hike index and create form

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,13 +11,21 @@ import ScenicShow from '../pages/ScenicShow'
 function Main(props) {
 
     const [runs, setRuns] = useState(null)
+    const [hikes, setHikes] = useState(null)
 
     const RunURL = "http://localhost:3000/run/"
+    const HikeURL = "http://localhost:3000/hike/"
 
     const getRuns = async () => {
         const response = await fetch(RunURL)
         const data = await response.json()
         setRuns(data)
+    }
+
+    const getHikes = async () => {
+        const response = await fetch(HikeURL)
+        const data = await response.json()
+        setHikes(data)
     }
 
     const createRuns = async (run) => {
@@ -29,7 +37,17 @@ function Main(props) {
         getRuns()
     }
 
+    const createHikes = async (hike) => {
+        await fetch(HikeURL, {
+            method: "post",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(hike)
+        })
+        getRuns()
+    }
+
     useEffect(() => getRuns(), [])
+    useEffect(() => getHikes(), [])
 
     return (
         <main>
@@ -41,7 +59,7 @@ function Main(props) {
                     <Run runs={runs} createRuns={createRuns} />
                 </Route>
                 <Route exact path="/hike">
-                    <Hike />
+                    <Hike hikes={hikes} createHikes={createHikes} />
                 </Route>
                 <Route exact path="/scenic">
                     <Scenic />
--- src/pages/Hike.js
+++ src/pages/Hike.js
@@ -1,5 +1,107 @@
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
+
 function Hike(props) {
-    return <h1>Hike</h1>
+
+    const [newForm, setNewForm] = useState({
+        title: "",
+        image: "",
+        distance: "",
+        location: "",
+        difficulty: "",
+        name: "",
+    })
+    
+    const handleChange = (event) => {
+        setNewForm({...newForm, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.createHikes(newForm)
+        setNewForm({
+            title: "",
+            image: "",
+            distance: "",
+            location: "",
+            difficulty: "",
+            name: "",
+        })
+    }
+
+    const loaded = () => {
+        return props.hikes.map((hike) => (
+
+                    <div key={hike._id} className="one-third column hikeIcons">
+                        <Link to={`/hike/${hike._id}`}>
+                            {/* <img src={hike.image} alt={hike.name} /> */}
+                            <h1>{hike.title}</h1>
+                        </Link>
+                    </div>
+
+        ))
+    }
+
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
+
+    return (
+        <section>
+            <form onSubmit={handleSubmit} className="container runForm">
+                <div className="row"><h2>Post A New Hike</h2></div>
+                <div className="row">
+                    <div className="six columns">
+                        <label for="title">Run Name</label>
+                        <input type="text" id="title" value={newForm.title} name="title" placeholder="Long Hike" onChange={handleChange} />
+                    </div>
+                    <div className="six columns">
+                        <label for="image">Image</label>
+                        <input type="text" id="image" value={newForm.image} name="image" placeholder="Image URL" onChange={handleChange} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="six columns">
+                        <label for="distance">Distance</label>
+                        <input type="text" id="distance" value={newForm.distance} name="distance" placeholder="20 Miles" onChange={handleChange} />
+                    </div>
+                    <div className="six columns">
+                        <label for="difficulty">Difficulty Level</label>
+                        <select id="difficulty" value={newForm.difficulty} name="difficulty" onChange={handleChange}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="six columns">
+                        <label for="location">Location</label>
+                        <input type="text" id="location" value={newForm.location} name="location" placeholder="Runtown, FL" onChange={handleChange} />
+                    </div>
+                    <div className="six columns">
+                        <label for="name">Your Name</label>
+                        <input type="text" id="name" value={newForm.name} name="name" placeholder="Joe Runner" onChange={handleChange} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="twelve columns">
+                        <input className="button-primary" type="submit" value="Post Hike" />
+                    </div>
+                </div>
+            </form>
+            <div className="container hikeIndex">
+                {props.hikes ? loaded() : loading()}
+            </div>
+        </section>
+    )
 }
 
 export default Hike
