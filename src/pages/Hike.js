0d3b0c05b42d6dd3f9d22737e53bd00fb555f6a7
gedunlap <g.e.dunlap90@gmail.com>
2021-05-22 14:32:11 -0400

    displays image for each instance on index page

--- src/pages/Hike.js
+++ src/pages/Hike.js
@@ -33,9 +33,9 @@ function Hike(props) {
         return props.hikes.map((hike) => (
 
                     <div key={hike._id} className="one-third column hikeIcons">
-                        <Link to={`/hike/${hike._id}`}>
-                            {/* <img src={hike.image} alt={hike.name} /> */}
-                            <h1>{hike.title}</h1>
+                        <Link to={`/hike/${hike._id}`} style={{textDecoration: 'none', color: 'teal'}}>
+                            <img src={hike.image} alt={hike.name} style={{width:'100%', borderRadius: '10px'}} />
+                            <h4>{hike.title}</h4>
                         </Link>
                     </div>
 
