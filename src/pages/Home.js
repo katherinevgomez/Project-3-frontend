32c9a9390175e6314103d7fc964d4ece143d262c
gedunlap <g.e.dunlap90@gmail.com>
2021-05-22 14:41:03 -0400

    changes displayed Scenic instances to Walk

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home(props) {
             <div className="row iconsRow">
                 <Link to="/run" className="one-third column routeIcon" id="run"><p >Run</p></Link>
                 <Link to="/hike" className="one-third column routeIcon" id="hike"><p>Hike</p></Link>
-                <Link to="/scenic" className="one-third column routeIcon" id="scenic"><p>Scenic Walk</p></Link>
+                <Link to="/scenic" className="one-third column routeIcon" id="scenic"><p>Walk</p></Link>
                 <p className="twelve columns instruction">Please Choose An Excursion</p>
                 <p className="twelve columns description">Welcome to "insert name here"! A collaborative effort bewteen people in motion, to share their favorite routes around the world.</p>
             </div>
