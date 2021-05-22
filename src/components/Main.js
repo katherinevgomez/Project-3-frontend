c01671edf8f86f822d3166739b6877e348f8b9b4
Katherine Gomez <kathygomez26@hotmail.com>
2021-05-22 10:52:54 -0400

    Revert "Merge pull request #5 from katherinevgomez/feature/add_maps"
    This reverts commit 3d50bacfcd0a45374595248d81adb73dece6552d, reversing
    changes made to 617459642ba0e1cca9036187efb966dfd67132cf.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,43 +1,21 @@
-import { useEffect, useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from '../pages/Home';
-import Run from '../pages/Run';
-import Hike from '../pages/Hike';
-import Scenic from '../pages/Scenic';
-import RunShow from '../pages/RunShow';
-import HikeShow from '../pages/HikeShow';
-import ScenicShow from '../pages/ScenicShow';
-
-const appData = {
-    runs: [
-        {
-            _id: '123',
-            location: '1600 Walnut Street, Philadelphia, PA',
-            title: 'Test Run',
-        },
-        {
-            _id: '321',
-            location: '2000 Broadway, New York, NY 10023',
-            title: 'Second Test Run',
-        },
-    ],
-};
+import {useEffect, useState} from 'react'
+import {Route, Switch} from 'react-router-dom'
+import Home from '../pages/Home'
+import Run from '../pages/Run'
+import Hike from '../pages/Hike'
+import Scenic from '../pages/Scenic'
+import RunShow from '../pages/RunShow'
+import HikeShow from '../pages/HikeShow'
+import ScenicShow from '../pages/ScenicShow'
 
 function Main(props) {
-    const [allData, setAllData] = useState({ isLoaded: false });
-    useEffect(() => {
-        setTimeout(() => {
-            setAllData({ ...appData, isLoaded: true });
-        }, 500);
-    }, [allData.isLoaded]);
-
     return (
         <main>
             <Switch>
                 <Route exact path="/">
                     <Home />
                 </Route>
-                <Route exact path="/run">
+                <Route path="/run">
                     <Run />
                 </Route>
                 <Route path="/hike">
@@ -46,24 +24,21 @@ function Main(props) {
                 <Route path="/scenic">
                     <Scenic />
                 </Route>
-                <Route
+                <Route 
                     path="/run/:id"
-                    render={(rp) => (
-                        <RunShow
-                            {...rp}
-                            runs={appData.runs}
-                            isLoaded={allData.isLoaded}
-                        />
-                    )}
+                    render={(rp) => ( <RunShow {...rp} /> )}
                 />
-                <Route path="/hike/:id" render={(rp) => <HikeShow {...rp} />} />
-                <Route
+                <Route 
+                    path="/hike/:id"
+                    render={(rp) => ( <HikeShow {...rp} /> )}
+                />
+                <Route 
                     path="/scenic/:id"
-                    render={(rp) => <ScenicShow {...rp} />}
+                    render={(rp) => ( <ScenicShow {...rp} /> )}
                 />
             </Switch>
         </main>
-    );
+    )
 }
 
-export default Main;
+export default Main
