00229fa37e789c5c54d25a301ccaccdf44c11c45
gedunlap <g.e.dunlap90@gmail.com>
2021-05-25 21:38:54 -0400

    reformat run show

--- src/pages/RunShow.js
+++ src/pages/RunShow.js
@@ -30,11 +30,11 @@ function Show(props) {
             <>
                 {/* hot display of details */}
                 <section className="container showPage">
-                    <div className="row twelve columns showImage" style={{marginBottom: '10px'}}>
+                    <div className="row twelve columns" style={{marginBottom: '10px'}}>
                         {run.image ? (
-                            <img
+                            <img id="showImage"
                                 src={run.image}
-                                style={{ maxWidth: "500px", maxHeight: "500px" }}
+                                style={{ maxWidth: "800px", maxHeight: "800px" }}
                                 alt={
                                     typeof editForm?.title === "string"
                                         ? editForm.title
@@ -83,32 +83,8 @@ function Show(props) {
                             )}
                         </div>
                     </div>
-                    <div className="row twelve columns" style={{height: '200px', marginBottom: '200px'}}>
-                        <div className="six columns showLocation">
-                            <p>Location:</p>
-                            {run.location && run.location !== "" ? (
-                                <h4>
-                                    {typeof editForm?.location === "string" ? (
-                                        <div>
-                                            <h4>{editForm.location}</h4>
-                                            <MapLoader
-                                                location={editForm.location}
-                                            ></MapLoader>
-                                        </div>
-                                    ) : (
-                                        <div>
-                                            <h4>{run.location}</h4>
-                                            <MapLoader
-                                                location={run.location}
-                                            ></MapLoader>
-                                        </div>
-                                    )}
-                                </h4>
-                            ) : (
-                                <h6>no location to display</h6>
-                            )}
-                        </div>
-                        <div className="six columns">
+                    <div className="row twelve columns" style={{height: '200px'}}>
+                    <div className="six columns showDifficulty">
                             <p>User Difficulty:</p>
                             {run.difficulty ? (
                                 <h4>
@@ -120,17 +96,37 @@ function Show(props) {
                                 <h6>no difficulty to display</h6>
                             )}
                         </div>
+                        <div className="six columns showLocation">
+                            <p>Location:</p>
+                            {run.location && run.location !== "" ? (
+                                <h4>
+                                    {typeof editForm?.location === "string"
+                                        ? editForm.location
+                                        : run.location}
+                                </h4>
+                            ) : (<h6>no location to display</h6>)}
+                        </div>
+                    </div>
+                    <div className="row twelve columns" style={{display: 'flex', justifyContent: "center"}}>
+                        <h4>
+                            <div>
+                                <MapLoader
+                                    location={run.location}
+                                ></MapLoader>
+                            </div>
+                        </h4>
                     </div>
                     
                 </section>
+                <h3 style={{textAlign: 'center', marginTop: '25px', fontWeight:'600'}}>Update Post Information Here</h3>
                 {/* edit form */}
                 <section className="container showUpdate" style={{marginTop: '25px'}}>
                     <form className="twelve columns" style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}
                         onChange={(event) => handleChange(event)}
                         onSubmit={(event) => processUpdate(event, run, run._id)}
                     >
-                        <div className="row twelve columns">
-                            <div className="six columns" style={{marginTop:'15px'}}>
+                        <div className="row twelve columns" style={{marginTop:'15px'}}>
+                            <div className="six columns">
                                 <label htmlFor="title">Title</label>
                                 <input
                                     type="text"
@@ -235,7 +231,7 @@ function Show(props) {
                         </div>
                         <br />
                         <div className="row">
-                            <button className="three columns updateSubmit" type="submit">Save Updates</button>
+                            <button style={{backgroundColor: 'white'}} type="submit">Save Updates</button>
                         </div>
                     </form>
                 </section>
