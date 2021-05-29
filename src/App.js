89924f896b8a1af3319d3d4bbe331af5cbe448e6
ianrackson <nasirus@protonmail.com>
2021-05-29 16:11:30 -0400

    katherine darkmode added

--- src/App.js
+++ src/App.js
@@ -23,6 +23,19 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
+    //darkmode
+    const [darkMode, setDarkmode] = React.useState(false);
+
+    React.useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add("dark");
+        } else {
+            document.body.classList.remove("dark");
+        }
+        const json = JSON.stringify(darkMode);
+        localStorage.setItem("site-dark-mode", json);
+        const currentMode = JSON.parse(json);
+    }, [darkMode]);
     // added lines 26-31
     // const token = getToken();
     // const [token, setToken] = useState();
@@ -72,6 +85,10 @@ function App() {
             <div className="row">
                 <div className="twelve columns">
                     <Header />
+                    {/*darkmode button*/}
+                    <button onClick={() => setDarkmode(!darkMode)}>
+                        Toggle Dark Mode
+                    </button>
                 </div>
             </div>
             {/* Added the following as well */}
