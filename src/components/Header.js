ca4b022baff3e11163dfbba3fe2cabe41f453c14
ianrackson <nasirus@protonmail.com>
2021-05-30 08:15:27 -0400

    remove dup nav bar - accidental duplication when merging with dev

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,15 @@
-import React from "react"
-import {Link} from "react-router-dom"
-import Navbar from './Nav/Navbar'
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Nav/Navbar";
 
 function Header(props) {
     return (
         <nav>
             <Navbar history={props.history} />
-            <Navbar />
         </nav>
-    )
+    );
 }
 
-export default Header
+export default Header;
 
 // className="nav"
